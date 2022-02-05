68166e52d76cbba5083943248b75235a1d7c1629
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2022-02-05 17:08:10 +0500

    Tasks List

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,13 +1,71 @@
 <template>
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card shadow">
-                <h3 class="text-muted p-3 border-bottom">Todo List</h3>
+            <div class="card shadow py-3">
+                <div class="row border-bottom">
+                    <div class="col-md-6">
+                        <h3 class="text-muted p-3 pt-0">Todo List</h3>
+                    </div>
+                    <div class="col-md-6">
+                        <input
+                            type="text"
+                            placeholder="Type to Search..."
+                            v-model="search"
+                            class="form-control"
+                        />
+                    </div>
+                </div>
+
+                <div class="d-flex m-3"></div>
+
+                <div class="m-3">
+                    <ul class="list-group">
+                        <li
+                            class="list-group-item d-flex justify-content-between align-items-center"
+                        >
+                            <span>Tasks</span>
+                            <span>Action</span>
+                        </li>
+                        <Todo
+                            v-for="task in tasks"
+                            :key="task.id"
+                            :task="task"
+                        />
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import Todo from "./Todo";
+export default {
+    components: {
+        Todo,
+    },
+    data() {
+        return {
+            search: "",
+            tasks: "",
+        };
+    },
+    created() {
+        this.getTasks();
+    },
+    methods: {
+        async getTasks() {
+            await axios
+                .get("/api/tasks")
+                .then((res) => {
+                    console.log(res);
+                    this.tasks = res.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+    },
+};
 </script>
---
+++ resources/js/Pages/Todo.vue
@@ -0,0 +1,26 @@
+<template>
+    <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+    >
+        <span>{{ task.name }}</span>
+        <div>
+            <span class="badge bg-primary cursor-pointer"> Edit </span>
+            <span class="badge bg-success cursor-pointer"> Update </span>
+            <span class="badge bg-danger cursor-pointer"> Delete </span>
+        </div>
+    </li>
+</template>
+<script>
+export default {
+    props: ["task"],
+    created() {
+        console.log(this.task);
+    },
+};
+</script>
+
+<style scoped>
+.cursor-pointer {
+    cursor: pointer;
+}
+</style>
